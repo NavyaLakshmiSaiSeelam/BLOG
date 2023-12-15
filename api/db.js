ec980930cf6e482109b64346d16632244abdcc51
Navya Lakshmi Sai Seelam <114072056+NavyaLakshmiSaiSeelam@users.noreply.github.com>
2023-12-15 18:35:24 +0530

    Update db.js

--- api/db.js
+++ api/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2"
 
 export const db = mysql.createConnection({
-  host:"localhost",
+  host:"",
   user:"root",
-  password: 'N@123456',
+  password: '',
   database:"blog"
 })
